b98b2e046e1727ddfeb4a3b4afb6c8826c086217
djturbo2002 <djturbo2002@gmail.com>
2017-04-06 20:25:06 +0200

    reparación fichero server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(function(req, res, next){
    if(req.headers['x-forwarded-proto'] === 'http') {
        next();
    }else{
-       res.redirect('http://'+ req.hostname + req.url);
+       res.redirect('http://'+ req.hostname + req.url );
    }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-    console.log('express server is up on port '+PORT');
+    console.log('express server is up on port '+PORT);
 });
