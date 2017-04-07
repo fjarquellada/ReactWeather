30d6066ea48f6a29a6dc01c5435c2ca5840018d5
djturbo2002 <djturbo2002@gmail.com>
2017-04-07 17:10:41 +0200

    centrando contenido

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,20 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 var Examples = (props) =>{
-    return(
-            <h3>Examples</h3>
+    return(  
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example laction to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Sevilla">Sevilla</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Madrid">Madrid</Link>
+                </li>
+            </ol>
+        </div>
         );
 };
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,25 +1,30 @@
 var React = require('react');
 var Nav = require('Nav');
-
-
+/*
 var Main = React.createClass({
     render: function(){
         return (
             <div>
                 <Nav/>
-                <h2>Main Component</h2>
-                {this.props.children}
+                <div className="row">
+                    <div className="columns medium-6 large-4 small-centered">
+                        {this.props.children}
+                    </div>
+                </div>
             </div>
         );
     }
 });
-
+*/
 var Main = (props) =>{
      return (
             <div>
                 <Nav/>
-                <h2>Main Component</h2>
-                {props.children}
+                <div className="row">
+                    <div className="columns medium-6 large-4 small-centered">
+                        {props.children}
+                    </div>
+                </div>
             </div>
         );
 };
